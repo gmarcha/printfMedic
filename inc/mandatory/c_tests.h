76a9133f1b2bb01522bee745b339952f405aa9be
gmarcha <gmarcha@github.com>
2021-09-10 12:18:36 +0200

    update header: add mandatory tests

---
+++ inc/mandatory/c_tests.h
@@ -0,0 +1,16 @@
+#ifndef C_TESTS_H
+# define C_TESTS_H
+
+# define NB_C_TESTS 4
+
+void test_c(int testIndex);
+
+int test_c_0(void);
+
+int test_c_space(void);
+
+int test_c_z(void);
+
+int test_c_space_c_0(void);
+
+#endif // C_TESTS_H
---
+++ inc/mandatory/percent_tests.h
@@ -0,0 +1,12 @@
+#ifndef PERCENT_TESTS_H
+# define PERCENT_TESTS_H
+
+# define NB_PERCENT_TESTS 2
+
+void test_pc(int testIndex);
+
+int test_percent(void);
+
+int test_percent_percent(void);
+
+#endif // PERCENT_TESTS_H
